19dea868dda437d5cd52f2b232147517d87a0c97
Imane-TOUIBA <touibaimane2003@gmail.com>
2024-07-31 22:55:39 +0200

    Podcast page

---
+++ ehcWebSite/resources/views/medias/podcast/podcats.blade.php
@@ -0,0 +1,377 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Binox HTML5 Template">
+
+  <title>EHC</title>
+
+  <!-- Fav Icon -->
+  <link rel="icon" type="image/x-icon" href="assets/imgs/logo/favicon.webp">
+
+
+  <!-- All CSS files -->
+  <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/icomon.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/swiper-bundle.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/progressbar.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/meanmenu.min.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/magnific-popup.css') }}">
+  <link rel="stylesheet" href="{{ url('assets/css/master-human.css') }}">
+  
+  <link rel="stylesheet" href="{{ url('assets/css/master-financial.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/master-service-style-1.css') }}">
+    
+   
+
+
+
+
+
+</head>
+
+
+<body class="font-heading-beatricetrial-regular">
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div id="container" class="container-preloader">
+      <div class="animation-preloader">
+        <div class="spinner"></div>
+        <div class="txt-loading">
+          <span data-text="E" class="characters">E</span>
+          <span data-text="H" class="characters">H</span>
+          <span data-text="C" class="characters">C</span>
+        </div>
+      </div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+  </div>
+
+  <!-- Cursor Animation -->
+  <div class="cursor1"></div>
+
+  <!-- Scroll to top -->
+  <div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98"></path>
+    </svg>
+  </div>
+
+  <!-- Switcher Area Start -->
+  <div class="switcher__area">
+    <div class="switcher__icon">
+      <!--<button id="switcher_open"><i class="fa-solid fa-gear"></i></button>-->
+	  <button id="switcher_open"><i class="fa-solid fa-robot"></i></button>
+      <!--<button id="switcher_close"><i class="fa-solid fa-xmark"></i></button>-->
+    </div>
+
+  <!-- offcanvas start  -->
+  <div class="offcanvas-3__area">
+    <div class="offcanvas-3__inner">
+      <div class="offcanvas-3__meta-wrapper">
+        <div class="">
+          <button id="close_offcanvas" class="close-button close-offcanvas" onclick="hideCanvas3()">
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div class="">
+          <div class="offcanvas-3__meta mb-145 d-none d-md-block">
+            <ul>
+              <li><a href="tel:+2-352698102" class="unnerline"><u>+2-352 698 102</u></a></li>
+              <li><a href="mailto:contact@me.com">contact@me.com</a></li>
+              <li><a href="">27 Division St, <br>
+                  New York, NY 10002, USA</a></li>
+            </ul>
+          </div>
+          <div class="offcanvas-3__social d-none d-md-block">
+            <p class="title">Follow Me</p>
+            <div class="offcanvas-3__social-links">
+              <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+              <a href=""><i class="fa-brands fa-twitter"></i></a>
+              <a href=""><i class="fa-brands fa-dribbble"></i></a>
+              <a href=""><i class="fa-brands fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="offcanvas-3__menu-wrapper">
+        <nav class="nav-menu offcanvas-3__menu">
+          <ul>
+            <li><a href="consultant-human.html">consultant-human</a></li>
+
+            <li><a href="about-2.html">about-2</a></li>
+
+            <li><a href="service-style-4.html">service style -4</a></li>
+
+            <li><a href="service-details.html">service details</a></li>
+
+            <li><a href="projects.html">project</a></li>
+
+            <li><a href="project-details.html">project details</a></li>
+            <li><a href="blog-style-1.html">blog style -1</a></li>
+            <li><a href="blog-details.html">blog details</a></li>
+            <li><a href="contact.html">contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <!-- offcanvas end  -->
+
+  <!-- search modal start -->
+  <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+    aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-body">
+          <form action="#" class="form-search">
+            <input type="text" placeholder="Search">
+			<button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>   <!--Recherche-icon-->
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- search modal end -->
+
+  @include('partials.header')
+  
+  <div class="has-smooth" id="has_smooth"></div>
+  <div id="smooth-wrapper">
+    <div id="smooth-content">
+      <div class="body-wrapper">
+
+        <!-- overlay switcher close  -->
+        <div class="overlay-switcher-close"></div>
+
+
+
+        <main>
+
+          <!-- blog area start  -->
+          <section class="blog-area section-spacing pin__area">
+            <div class="container">
+              <div class="blog-area-inner">
+                <div class="btn-wrapper has_fade_anim pin__element" data-fade-from="left">
+                  <a class="cf_btn wc-btn-underline btn-text-flip newsletter-btn" href="contact.html"><span
+                      data-text="Join our newsletter">Join our newsletter</span><i
+                      class="fa-solid fa-caret-right"></i></a>
+                </div>
+                <div class="blog-main-wrapper">
+                  <div class="section-heading">
+                    <div class="section-title-wrapper">
+                      <div class="title-wrapper has_fade_anim">
+                        <h1 class="section-title large">Our recent journal insight the binox</h1>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="blog-wrapper-area fix has_fade_anim" data-delay=".35">
+                    <div class="blog-wrapper">
+                      <a href="blog-details.html">
+                        <article class="blog style-2">
+                          <div class="author">
+                            <div class="avatar"><img src="assets/imgs/client/img-s-6.webp" alt=""></div>
+                            <div class="">
+                              <span class="meta-title">Written by</span>
+                              <span class="name">Binox Agency</span>
+                            </div>
+                          </div>
+                          <div class="content-wrapper">
+                            <div class="content">
+                              <div class="meta">
+                                <span class="tag">Business</span>
+                                <span class="date has-left-line">January 29, 2020</span>
+                              </div>
+                              <h2 class="title">How to create your purpose-filled career
+                                comfortable clothes</h2>
+                              <p class="text">Over the past three years, marketers have faced journey due to the rapid
+                                shifts
+                                in consumer.....</p>
+                            </div>
+                            <div class="btn-wrapper">
+                              <span class="cf_btn wc-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+                            </div>
+                          </div>
+                        </article>
+                      </a>
+                      <a href="blog-details.html">
+                        <article class="blog style-2">
+                          <div class="author">
+                            <div class="avatar"><img src="assets/imgs/client/img-s-7.webp" alt=""></div>
+                            <div class="">
+                              <span class="meta-title">Written by</span>
+                              <span class="name">Binox Agency</span>
+                            </div>
+                          </div>
+                          <div class="content-wrapper">
+                            <div class="content">
+                              <div class="meta">
+                                <span class="tag">Business</span>
+                                <span class="date has-left-line">January 29, 2020</span>
+                              </div>
+                              <h2 class="title">Sitting with your basic goodness
+                                top signs that time.</h2>
+                              <p class="text">Over the past three years, marketers have faced journey due to the rapid
+                                shifts
+                                in consumer.....</p>
+                            </div>
+                            <div class="btn-wrapper">
+                              <span class="cf_btn wc-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+                            </div>
+                          </div>
+                        </article>
+                      </a>
+                      <a href="blog-details.html">
+                        <article class="blog style-2">
+                          <div class="author">
+                            <div class="avatar"><img src="assets/imgs/client/img-s-8.webp" alt=""></div>
+                            <div class="">
+                              <span class="meta-title">Written by</span>
+                              <span class="name">Binox Agency</span>
+                            </div>
+                          </div>
+                          <div class="content-wrapper">
+                            <div class="content">
+                              <div class="meta">
+                                <span class="tag">Business</span>
+                                <span class="date has-left-line">January 29, 2020</span>
+                              </div>
+                              <h2 class="title">Understanding behavioral biases and making
+                                rational way</h2>
+                              <p class="text">Over the past three years, marketers have faced journey due to the rapid
+                                shifts
+                                in consumer.....</p>
+                            </div>
+                            <div class="btn-wrapper">
+                              <span class="cf_btn wc-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+                            </div>
+                          </div>
+                        </article>
+                      </a>
+                      <a href="blog-details.html">
+                        <article class="blog style-2">
+                          <div class="author">
+                            <div class="avatar"><img src="assets/imgs/client/img-s-9.webp" alt=""></div>
+                            <div class="">
+                              <span class="meta-title">Written by</span>
+                              <span class="name">Binox Agency</span>
+                            </div>
+                          </div>
+                          <div class="content-wrapper">
+                            <div class="content">
+                              <div class="meta">
+                                <span class="tag">Business</span>
+                                <span class="date has-left-line">January 29, 2020</span>
+                              </div>
+                              <h2 class="title">Understanding behavioral biases and making
+                                rational way</h2>
+                              <p class="text">Over the past three years, marketers have faced journey due to the rapid
+                                shifts
+                                in consumer.....</p>
+                            </div>
+                            <div class="btn-wrapper">
+                              <span class="cf_btn wc-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+                            </div>
+                          </div>
+                        </article>
+                      </a>
+                      <a href="blog-details.html">
+                        <article class="blog style-2">
+                          <div class="author">
+                            <div class="avatar"><img src="assets/imgs/client/img-s-10.webp" alt=""></div>
+                            <div class="">
+                              <span class="meta-title">Written by</span>
+                              <span class="name">Binox Agency</span>
+                            </div>
+                          </div>
+                          <div class="content-wrapper">
+                            <div class="content">
+                              <div class="meta">
+                                <span class="tag">Business</span>
+                                <span class="date has-left-line">January 29, 2020</span>
+                              </div>
+                              <h2 class="title">Understanding behavioral biases and making
+                                rational way</h2>
+                              <p class="text">Over the past three years, marketers have faced journey due to the rapid
+                                shifts
+                                in consumer.....</p>
+                            </div>
+                            <div class="btn-wrapper">
+                              <span class="cf_btn wc-btn-icon"><i class="fa-solid fa-arrow-right-long"></i></span>
+                            </div>
+                          </div>
+                        </article>
+                      </a>
+                    </div>
+                  </div>
+                  <ul class="pagination style-1 has_fade_anim">
+                    <li><a href="#">1</a></li>
+                    <li><a class="current" href="#">2</a></li>
+                    <li><a href="#">Next <img src="assets/imgs/icon/arrow-next-icon.webp" alt="arrow-icon"></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- blog area end  -->
+
+          <!-- cta area start  -->
+          <div class="cta-area style-1 mbm-1">
+            <div class="container container-large">
+              <div class="cta-area-wrapper">
+                <div class="cta-left">
+                  <ul class="cta-info">
+                    <li>
+                      <p class="title">Saturday - Thursday :</p>
+                      <span>8:30 am - 10:45 pm</span>
+                    </li>
+                  </ul>
+                </div>
+                <div class="cta-right">
+                  <div class="btn-wrapper">
+                    <a class="cf_btn wc-btn-underline btn-text-flip" href="contact.html"><span
+                        data-text="Call for an appointment">Call for an appointment</span><img
+                        src="assets/imgs/icon/arrow-triangle.webp" alt=""></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- cta area end  -->
+
+        </main>
+
+        @include('partials.footer')
+
+
+
+  <!-- All JS files -->
+  <script src="assets/js/jquery-3.6.0.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="assets/js/swiper-bundle.min.js"></script>
+  <script src="assets/js/counter.js"></script>
+  <script src="assets/js/progressbar.js"></script>
+  <script src="assets/js/gsap.min.js"></script>
+  <script src="assets/js/ScrollSmoother.min.js"></script>
+  <script src="assets/js/ScrollTrigger.min.js"></script>
+  <script src="assets/js/jquery.meanmenu.min.js"></script>
+  <script src="assets/js/backToTop.js"></script>
+  <script src="assets/js/main.js"></script>
+  <script src="assets/js/error-handling.js"></script>
+  <script src="assets/js/wc-cursor.js"></script>
+  <script src="assets/js/offcanvas.js"></script>
+
+
+
+</body>
+
+</html>
