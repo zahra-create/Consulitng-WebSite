f35bb8929a8aeac728543b8d93e7e6f376f19ff5
Imane-TOUIBA <touibaimane2003@gmail.com>
2024-08-05 17:12:32 +0200

    menu bar

--- ehcWebSite/resources/views/contact.blade.php
+++ ehcWebSite/resources/views/contact.blade.php
@@ -105,56 +105,7 @@
   <!-- Switcher Area End -->
 
   <!-- offcanvas start  -->
-  <div class="offcanvas-3__area">
-    <div class="offcanvas-3__inner">
-      <div class="offcanvas-3__meta-wrapper">
-        <div class="">
-          <button id="close_offcanvas" class="close-button close-offcanvas" onclick="hideCanvas3()">
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-        <div class="">
-          <div class="offcanvas-3__meta mb-145 d-none d-md-block">
-            <ul>
-              <li><a href="tel:+2-352698102" class="unnerline"><u>+2-352 698 102</u></a></li>
-              <li><a href="mailto:contact@me.com">contact@me.com</a></li>
-              <li><a href="">27 Division St, <br>
-                  New York, NY 10002, USA</a></li>
-            </ul>
-          </div>
-          <div class="offcanvas-3__social d-none d-md-block">
-            <p class="title">Follow Me</p>
-            <div class="offcanvas-3__social-links">
-              <a href=""><i class="fa-brands fa-facebook-f"></i></a>
-              <a href=""><i class="fa-brands fa-twitter"></i></a>
-              <a href=""><i class="fa-brands fa-dribbble"></i></a>
-              <a href=""><i class="fa-brands fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="offcanvas-3__menu-wrapper">
-        <nav class="nav-menu offcanvas-3__menu">
-          <ul>
-          <li><a href="{{route('about')}}">A propos</a></li>
-            <li><a href="{{ route('service-entreprise') }}"> Services Entreprises</a></li>
-            <li><a href="{{ route('service-fonctionnaire') }}">Services Salariés</a></li>
-            <li><a href="{{ route('service-etudiant') }}">Services Etudiants</a></li>
-            <li><a href="consultant-financial.html">Offres d'Emploi</a></li>
-            <li><a href="consultant-human.html">Stages</a></li>
-            <li><a href="consultant-insurance.html">Candidature spontannee</a></li>
-            <li><a href="consultant-it.html">Blog</a></li>
-            <li><a href="{{ route('playlists') }}">Videos</a></li>
-            <li><a href="{{ route('Podcasts') }}">Podcasts</a></li>
-            <li><a href="{{ route('contact') }}">Contact</a></li>
-            <li><a href="{{ route('demande-devis') }}">Demander devis</a></li>
-            <li><a href="about-1.html">Payer prestation</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </div>
+  @include('partials.menu-bar')
   <!-- offcanvas end  -->
 
   <!-- search modal start -->
